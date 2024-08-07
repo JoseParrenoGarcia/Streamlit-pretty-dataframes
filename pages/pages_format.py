a464e68f71df23f457d1084336355330eae1dd54
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-08-07 08:45:37 +0200

    Adding coming soon

--- pages/pages_format.py
+++ pages/pages_format.py
@@ -6,7 +6,7 @@ def pages_format():
         [
             Page("streamlit_app.py", "Home", "🏠", in_section=False),
             Page("pages/styler.py", "Styler objects", "🎨", in_section=False),
-            Page("pages/adgrid.py", "AdGrid", "🌐", in_section=False),
+            Page("pages/aggrid.py", "AdGrid", "🌐", in_section=False),
         ]
     )
     add_indentation()   # Function that looks at the in_section parameter
