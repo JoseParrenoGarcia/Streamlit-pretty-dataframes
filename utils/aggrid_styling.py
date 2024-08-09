41ddc1931ac667d83cdbda765608d2bae91fccd2
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-08-09 18:31:58 +0200

    Correct JsCode formatting

--- utils/aggrid_styling.py
+++ utils/aggrid_styling.py
@@ -1,59 +1,47 @@
 from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
 
-
-def get_numeric_style_with_precision(column_name: str,
-                                     decimal_points: int = None,
-                                     currency: bool = False,
-                                     percentage: bool = False,
-                                     ) -> dict:
-    if currency:
-        return {"type": ["numericColumn", "customNumericFormat"],
-                "valueGetter": f"data.{column_name}.toLocaleString('en-US', {{style: 'currency', currency: 'USD', maximumFractionDigits: {decimal_points}}})",
-                }
-
-
-    elif percentage:
-        PERCENTAGE_FORMATTER = JsCode(
-            f"""
-            function(params) {{
-                            if (params.value === null || params.value === undefined) {{
-                                return '';
-                            }}
-                            return (params.value * 100).toLocaleString('en-US', {{minimumFractionDigits: {decimal_points}, maximumFractionDigits: {decimal_points}}}) + '%';
-                        }}
-            """
-        )
-
-        return {"type": ["numericColumn", "customNumericFormat"],
-                "valueFormatter": PERCENTAGE_FORMATTER,
-                }
-
-    else:
-        THOUSAND_COMMA_SEPARATOR = JsCode(
-            f"""
-            function(params) {{
-                            if (params.value === null || params.value === undefined) {{
-                                return '';
-                            }}
-                            return Number(params.value).toLocaleString('en-US');
-                            }}
-            """
-        )
-
-        return {"type": ["numericColumn", "customNumericFormat"],
-                "valueFormatter": THOUSAND_COMMA_SEPARATOR,
-                }
-
-
-# Define serializable column formatting
-column_formatting = {
-        'Country': ('Country', {}),
-        'Period_1': ('Period 1', {**get_numeric_style_with_precision(column_name='Period_1', decimal_points=0, currency=True)}),
-        'Period_2': ('Period 2', {**get_numeric_style_with_precision(column_name='Period_2', decimal_points=0, currency=True)}),
-        'Difference': ('Difference', {**get_numeric_style_with_precision(column_name='Difference', decimal_points=0, currency=True)}),
-        'Percentage Change': ('Percentage', {**get_numeric_style_with_precision(column_name='Percentage', decimal_points=0, percentage=True)}),
-        'Percentage Change rank': ('Percentage Change rank', {}),
+# Define JsCode for percentage formatting
+percentage_formatter = JsCode("""
+function(params) {
+    if (params.value == null) {
+        return '';
     }
+    var decimalPoints = params.column.colDef.cellRendererParams.decimalPoints || 2;
+    return (params.value * 100).toFixed(decimalPoints) + '%';
+}
+""")
+
+percentage_getter = JsCode("""
+function(params) {
+    return params.data[params.colDef.field];
+}
+""")
+
+# Define JsCode for currency formatting
+currency_formatter = JsCode("""
+function(params) {
+    if (params.value == null || params.value === undefined) {
+        return '';
+    }
+    var decimalPoints = params.column.colDef.cellRendererParams.decimalPoints || 0;
+    var currencySymbol = params.column.colDef.cellRendererParams.currencySymbol || '€';
+    var value = params.value;
+
+    // Format the number with thousand separators and decimal points
+    var formattedNumber = value.toLocaleString('en-US', {
+        minimumFractionDigits: decimalPoints,
+        maximumFractionDigits: decimalPoints
+    });
+
+    return currencySymbol + formattedNumber;
+}
+""")
+
+currency_getter = JsCode("""
+function(params) {
+    return params.data[params.colDef.field];
+}
+""")
 
 
 grid_options = {
@@ -68,19 +56,56 @@ def aggrid_cells_formatting(df):
 
     grid_builder.configure_grid_options(**grid_options)
 
+    grid_builder.configure_default_column(filter=True)
+
     # https://streamlit-aggrid.readthedocs.io/en/docs/GridOptionsBuilder.html#st_aggrid.grid_options_builder.GridOptionsBuilder.configure_column
     # https://www.ag-grid.com/javascript-data-grid/column-properties/
-    for input_column_name, (clean_name, style_dict) in column_formatting.items():
-        grid_builder.configure_column(input_column_name, header_name=clean_name, **style_dict)
+
+    # The value formatter is only used for adding symbols or formatting stuff.
+    # The data used for filter will not be from the valueFormatter.
+    # It will actually be from the valueGetter.
+
+    grid_builder.configure_column('Percentage Change',
+                                  header_name='Percentage Change (%)',
+                                  type=['numericColumn', 'numberColumnFilter', 'customNumericFormat'],
+                                  valueGetter=percentage_getter,
+                                  valueFormatter=percentage_formatter,
+                                  cellRendererParams={'decimalPoints': 1}
+                                  )
+
+    grid_builder.configure_column('Period_1',
+                                  header_name='Period 1',
+                                  type=['numericColumn', 'numberColumnFilter', 'customNumericFormat'],
+                                  valueGetter=currency_getter,
+                                  valueFormatter=currency_formatter,
+                                  cellRendererParams={'decimalPoints': 0, 'currencySymbol': '€'}
+                                  )
+
+    grid_builder.configure_column('Period_2',
+                                  header_name='Period 2',
+                                  type=['numericColumn', 'numberColumnFilter', 'customNumericFormat'],
+                                  valueGetter=currency_getter,
+                                  valueFormatter=currency_formatter,
+                                  cellRendererParams={'decimalPoints': 0, 'currencySymbol': '€'}
+                                  )
+
+    grid_builder.configure_column('Difference',
+                                  header_name='Difference',
+                                  type=['numericColumn', 'numberColumnFilter', 'customNumericFormat'],
+                                  valueGetter=currency_getter,
+                                  valueFormatter=currency_formatter,
+                                  cellRendererParams={'decimalPoints': 0, 'currencySymbol': '€'}
+                                  )
 
     # Build grid options
     gridOptions = grid_builder.build()
 
+    grid_response = AgGrid(df,
+                           gridOptions=gridOptions,
+                           allow_unsafe_jscode=True,
+                           height=min(500, (len(df)) * 38),  # 38px per row or 500px
+                           fit_columns_on_grid_load=False,
+                           theme='balham')  # Theme can be changed
+
     # https://streamlit-aggrid.readthedocs.io/en/docs/AgGrid.html
-    return AgGrid(df,
-                  gridOptions=gridOptions,
-                  allow_unsafe_jscode=True,
-                  height=min(500, (len(df))*38),  # 38px per row or 500px
-                  fit_columns_on_grid_load=False,
-                  theme='balham',  # 'streamlit', 'balham', 'alpine', 'material'
-                  )
+    return grid_response
