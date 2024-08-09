1265a2d0f287752f6488a97a505a1a8f176305e6
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-08-09 19:52:35 +0200

    Update pages_format.py

--- pages/pages_format.py
+++ pages/pages_format.py
@@ -4,9 +4,8 @@ from st_pages import Page, Section, add_indentation, show_pages
 def pages_format():
     show_pages(
         [
-            Page("streamlit_app.py", "Home", "🏠", in_section=False),
-            Page("pages/styler.py", "Styler objects", "🎨", in_section=False),
-            Page("pages/aggrid.py", "AdGrid", "🌐", in_section=False),
+            Page("streamlit_app.py", "Home", "🏠"),
+            Page("pages/styler.py", "Styler objects", "🎨"),
+            Page("pages/aggrid.py", "AdGrid", "🌐"),
         ]
     )
-    add_indentation()   # Function that looks at the in_section parameter
